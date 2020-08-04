f66e214628d7092d9c17137548433e08fdb521c0
Erik Högberg <erik.hogberg.9945@student.uu.se>
2020-08-04 16:10:18 +0200

    breaking platforms working

---
+++ Unity/Brackeys2020_2-NoTimeForJam/Assets/Scripts/BreakingPlatform.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakingPlatform : Triggerable
+{
+	private void OnCollisionExit2D(Collision2D other)
+	{
+		Debug.Log("breaking platform collision exit something");
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Debug.Log("breaking platform collision exit player");
+			gameObject.SetActive(false);
+		}
+	}
+
+	public override void Press()
+	{
+		gameObject.SetActive(true);
+	}
+	public override void Release()
+	{
+	}
+
+}
